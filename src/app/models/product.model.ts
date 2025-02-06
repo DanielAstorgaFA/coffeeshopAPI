c38fd054801989201b52bc0510e1e70ef27c8579
DanielAstorgaFA <daniel.astorga.lopez.alu@iesfernandoaguilar.es>
2025-02-06 21:35:52 +0100

    Primer commit

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,7 @@
+export interface IProduct { //Cambiado los campos de producto por el café:
+    id:          number;
+    title:       string;
+    ingredients: string[]
+    description: string;
+    image:       string;
+}
